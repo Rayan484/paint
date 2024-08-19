d850d22ccc8a2e1a79c97d9229f2fe40ec7728ef
Muhammad Rayan <75950217+Rayan484@users.noreply.github.com>
2024-08-19 16:43:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -69,5 +69,5 @@ def getembedding1():
     except Exception as e:
         return flask.jsonify({"error": str(e)})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
